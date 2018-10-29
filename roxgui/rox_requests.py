7de8be39bfee2d97b5cd35d6d6148199d26a6b14
Jana Becker <becker@droxit.de>
2018-10-29 14:30:01 +0100

    added function to get a list of services as dictionary

---
+++ roxgui/rox_requests.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+#
+# commands.py
+#
+# devs@droxit.de - droxIT GmbH
+#
+# Copyright (c) 2018 droxIT GmbH
+#
+
+import json
+import os
+from user_settings import ROX_DIR, SERVICES_DIR, ROX_URL
+
+#get path to composer folder, url, services directory
+services_dir = SERVICES_DIR
+roxconnector = ROX_URL
+rox_dir = ROX_DIR
+
+
+#get a list of all available services in the services directory
+def get_service_list():
+    available_services = {}
+    for f in os.scandir(services_dir):
+        if f.is_file() and f.name.endswith('.json'):
+            service_args = json.load(f)
+            available_services[f.name] = service_args
+    return available_services
+
+#post message to pipeline
+def post_to_pipeline(*args):
+    pass
+
+#get all running services ?
+#def get_services():
+#    pass
+
+#start a new service
+def start_service(*args):
+    pass
+
+#create a new pipeline
+def set_pipeline(*args):
+    pass
