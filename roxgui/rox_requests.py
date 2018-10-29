1f4e2843e65fed79f88e5743a186450bfe2039cd
Jana Becker <becker@droxit.de>
2018-10-29 16:08:54 +0100

    added functionality to interact with roxconnector

--- roxgui/rox_requests.py
+++ roxgui/rox_requests.py
@@ -9,6 +9,7 @@
 
 import json
 import os
+import requests
 from user_settings import ROX_DIR, SERVICES_DIR, ROX_URL
 
 #get path to composer folder, url, services directory
@@ -33,14 +34,54 @@ def get_service_list():
 def post_to_pipeline(*args):
     pass
 
-#get all running services ?
-#def get_services():
-#    pass
 
 #start a new service
-def start_service(*args):
-    pass
+def start_service(service_json):
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        r = requests.post('http://{}/start_service'.format(roxconnector), json=service_json, headers=headers)
+    except requests.exceptions.ConnectionError as e:
+        return "ERROR: no connection to server - {}".format(e)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'ERROR: {} - {}'.format(r.status_code, r.text)
+
+#get all registered services
+def get_services():
+    try:
+        r = requests.get('http://{}/services'.format(roxconnector))
+    except requests.exceptions.ConnectionError as e:
+        return "ERROR: no connection to server - {}".format(e)
+
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'ERROR: {} - {}'.format(r.status_code, r.text)
 
 #create a new pipeline
-def set_pipeline(*args):
-    pass
+def set_pipeline(pipename, services):
+    if len(args) < 2:
+        return 'ERROR: a pipeline name and at least one service must be specified'
+    pipename = args[0]
+    services = args[1:]
+    d = {'name': pipename, 'services': services}
+    headers = {'Content-Type': 'application/json'}
+    try:
+        r = requests.post('http://{}/set_pipeline'.format(roxconnector), data=json.dumps(d), headers=headers)
+    except requests.exceptions.ConnectionError as e:
+        return "ERROR: no connection to server - {}".format(e)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'ERROR: {} - {}'.format(r.status_code, r.text)
+
+#get registered pipelines
+def get_pipelines(*args):
+    r = requests.get('http://{}/pipelines'.format(roxconnector))
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'ERROR: {} - {}'.format(r.status_code, r.text)
+
