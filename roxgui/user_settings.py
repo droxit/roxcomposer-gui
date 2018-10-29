19733f2ee4336cadaa635012ade8545b6cf0ebff
Christian Hecktor <hecktor@droxit.de>
2018-10-29 16:10:57 +0100

    First draft to start service via web page

---
+++ roxgui/user_settings.py
@@ -0,0 +1,3 @@
+ROX_DIR = ""
+SERVICES_DIR = "/home/christian/Projekte/roxcomposer/cli/services/"
+ROX_URL = ""
--- roxgui/web/views.py
+++ roxgui/web/views.py
@@ -1,9 +1,20 @@
 """Configuration of web views."""
 
+import rox_requests
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
 def web_main(request):
     """Main page."""
-    context = {}
-    return render(request, "web/web.html", context)
+    if request.method == 'GET':
+        response_dict = rox_requests.get_service_list()
+        service_names = []
+        service_jsons = []
+        for s_name, s_json in response_dict.items():
+            service_names.append(s_name)
+            service_jsons.append(s_json)
+        context = {"names": service_names, "jsons": service_jsons}
+        return render(request, "web/web.html", context)
+    elif request.method == "POST":
+        return HttpResponseNotFound(request["services"])
