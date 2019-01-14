0971dc4e39f3441608d23ca4a9996cd170d87428
Jana Becker <becker@droxit.de>
2019-01-14 10:09:21 +0100

    added convert to json function for roxresponse since it is needed sometimes for debugging

--- roxgui/rox_response.py
+++ roxgui/rox_response.py
@@ -47,3 +47,15 @@ class RoxResponse:
     def __str__(self):
         return "RoxResponse status: {}, Msg: {}, Data: {} ".format(self.success, self.message, self.data)
 
+    def convert_to_json(self) -> dict:
+        """
+        Convert current instance to JSON dictionary.
+        :return: Current instance as JSON dictionary.
+        """
+        json_dict = {
+            "success": self.success,
+            "message": self.message,
+            "data": self.data,
+            "error_data": self.error_data
+        }
+        return json_dict
