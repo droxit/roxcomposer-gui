c0d1323cf95c15f5411957a5f67990ed2832225b
Jana Becker <becker@droxit.de>
2019-01-14 11:07:37 +0100

    added filter, sort and search functions

--- roxgui/web/static/web/js/functions.js
+++ roxgui/web/static/web/js/functions.js
@@ -2,15 +2,23 @@ function filter(filterList, condition) {
     return filterList.filter(condition);
 }
 
-function sort() {
-
+function sort(sortList, condition) {
+    return sortList.sort(condition);
 }
 
 function search(searchList, item) {
     // returns a list of elements matching the item
     return filter(searchList, function(itemInList){
-        return item == itemInList;
+        return itemInList.includes(item);
+    });
+
+}
+
+function sortByAlphabet(sortList){
+    return sort(sortList, function(a, b){
+        if(a < b) {return -1; }
+        if(a > b) {return 1; }
+        return 0;
     });
 }
 
-console.log(search(['bla','blub'], 'bla'))
