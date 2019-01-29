2b6480914ec827fc82fe1a89e1afca206be93d6c
Jana Becker <becker@droxit.de>
2019-01-29 15:09:57 +0100

    added comments

--- roxgui/web/views/pipe_views.py
+++ roxgui/web/views/pipe_views.py
@@ -16,6 +16,11 @@ from web.views.json_views import create_rox_response
 
 @require_http_methods(["POST"])
 def get_pipelines(request):
+    """
+    Get a json with all currently available pipelines from the ROXcomposer
+    :param request: no data needed
+    :return: JsonResponse containing the json of pipes
+    """
     # Get JSON data of local pipelines.
     result = rox_request.get_pipelines()
     return JsonResponse(result.data)
@@ -23,6 +28,12 @@ def get_pipelines(request):
 
 @require_http_methods(["POST"])
 def create_pipeline(request):
+    """
+    Create a new pipe on the ROXcomposer.
+    Note that pipelines with the same name will be overwritten without prompt.
+    :param request: shall contain a 'services' list and 'pipe_name' which is the name of the new pipe
+    :return: The RoxResponse
+    """
     # Get the service list that the pipe is supposed to contain
     services = request.POST.getlist("services[]", default=[])
     pipe_name = request.POST.get("pipe_name", default="")
@@ -32,10 +43,14 @@ def create_pipeline(request):
 
 @require_http_methods(["POST"])
 def get_pipeline_info(request):
+    """
+    Retrieve the pipeline information for a pipe name
+    :param request: shall contain 'pipe_name', a string
+    :return: a RoxResponse containing the pipe information
+    """
     # Get the pipe name
     pipe_name = request.POST.get("pipe_name", default="")
     result = rox_request.get_pipelines() #get a list of all available pipelines
-    print("PIPES: ", result.data)
     if pipe_name in result.data:
         result.data = result.data[pipe_name]
 
@@ -43,6 +58,11 @@ def get_pipeline_info(request):
 
 @require_http_methods(["POST"])
 def send_msg(request):
+    """
+    Send a message to a pipe
+    :param request: Shall contain 'pipe' a string with the pipeline name, 'msg' a string that is the message
+    :return: A RoxResponse with the response message (and status) from the server
+    """
     # Get the message and the pipe name
     pipe_name = request.POST.get("pipe", default="")
     msg = request.POST.get("msg", default="")
