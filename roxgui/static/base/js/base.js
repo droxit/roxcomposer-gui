f15671b9f59563d62df48d94e83272ba52110571
Jana Becker <becker@droxit.de>
2019-02-18 09:49:15 +0100

    added empty function that should send a post request to the server to check if the roxcomposer is running and the path and ports are set and if not open a modal with the corresponding text and inputs.

--- roxgui/static/base/js/base.js
+++ roxgui/static/base/js/base.js
@@ -11,3 +11,18 @@
 $('[data-toggle="tooltip"]').tooltip({ // Enable bootstrap tooltips
 	container: 'body'
 });
+
+
+/* This function is called on every website to check if the roxcomposer is running.
+    If it is not running it will open a modal on the current website that prompts the user to
+    enter a Path to the ROXcomposer's installation folder and the port on which the ROXcomposer is running.*/
+function check_rox_running(){
+    var CSRFtoken = $('input[name=csrfmiddlewaretoken]').val();
+	$.post("check_rox", {
+		csrfmiddlewaretoken: CSRFtoken
+	}).done(function(data) {
+
+		// do things
+
+	});
+}
