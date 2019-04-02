b8c2e3673c2a812c19989a639a7e41d89ec56f71
Jana Becker <becker@droxit.de>
2019-04-02 15:42:22 +0200

    test

--- roxgui/web/static/web/js/rox_pipelines.js
+++ roxgui/web/static/web/js/rox_pipelines.js
@@ -68,7 +68,7 @@ function set_pipe_info(elem) {
 function add_search_bar() {
 	var search_container = $("#search-bar-container");
 	var search_btn_container = $("#search-btn-container");
-	var searchbar = $("<input id='search_services' class='form-control' type='text' onchange='$(\"#btn-add-service\")[0].click()' list='data-service-list' placeholder='Add service ...' disabled='disabled'></input>");
+	var searchbar = $("<input id='search_services' class='form-control' type='text'  list='data-service-list' placeholder='Add service ...' disabled='disabled'></input>");
 	var service_datalist = $("<datalist id='data-service-list'></datalist>");
 	var add_btn = $("<button type='button' id='btn-add-service' class='btn btn-primary btn-circle-big disabled' style='margin-left:-20px' onclick='add_service_to_pipe()'><span class='fas fa-plus'></span></button>")
 
